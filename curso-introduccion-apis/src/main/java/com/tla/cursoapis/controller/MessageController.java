3de4d19a57f0caf7700caa7f2d439161a97a4cba
juanprint <juan_alonso_7@hotmail.com>
2026-02-27 13:56:37 -0500

    crud sencillo

--- curso-introduccion-apis/src/main/java/com/tla/cursoapis/controller/MessageController.java
+++ curso-introduccion-apis/src/main/java/com/tla/cursoapis/controller/MessageController.java
@@ -7,7 +7,12 @@ package com.tla.cursoapis.controller;
 import com.tla.cursoapis.entity.Message;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +32,26 @@ public class MessageController {
     @GetMapping
     public List<Message> listarMensajes(){
         return mensajes;
+    }
+    @GetMapping("/{id}")//recibe el id y lo pasa como parametro 
+                                     //variablde la ruta
+    public Message obtenerMessagePorId(@PathVariable int id){
+                    //transforma a un flujo de datos
+        Optional<Message> message=mensajes.stream()
+                .filter(m->m.getId()==id)
+                .findFirst();
+        //retorna message y sino existe devuelve null para eso esta el optional
+        return message.orElse(null);
+    }
+    //Enviar,Crear data
+    @PostMapping
+    public Message crearMensaje(@RequestBody Message message){
+        mensajes.add(message);
+        return message;
+    }
+    @DeleteMapping("/{id}")
+    public void eliminarMensaje(@PathVariable int id){
+                //elimar si esta...
+        mensajes.removeIf(m->m.getId()==id);
     }
 }
